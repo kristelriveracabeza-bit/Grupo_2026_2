b22313a365f4e34820f371879dcd4c4e1565593b
kristelriveracabeza-bit <kristelriveracabeza@gmail.com>
2026-07-13 00:12:30 -0500

    Feature: output en security
    El output en el modulo security

--- modules/security/output.tf
+++ modules/security/output.tf
@@ -1,6 +1,7 @@
 
 # OUTPUTS DE ROLES IAM
 
+
 output "lambda_role_arn" {
   description = "ARN del rol IAM de Lambda"
   value       = aws_iam_role.lambda.arn
@@ -41,16 +42,18 @@ output "cognito_role_name" {
   value       = aws_iam_role.cognito.name
 }
 
+
 # OUTPUTS DE POLÍTICAS IAM
+
 
 output "lambda_policy_arns" {
   description = "ARNs de las políticas de Lambda"
   value = {
-    sqs         = aws_iam_policy.lambda_sqs.arn
-    sns         = aws_iam_policy.lambda_sns.arn
-    secrets     = aws_iam_policy.lambda_secrets.arn
-    dynamodb    = aws_iam_policy.lambda_dynamodb.arn
-    logs        = aws_iam_policy.lambda_logs.arn
+    sqs      = aws_iam_policy.lambda_sqs.arn
+    sns      = aws_iam_policy.lambda_sns.arn
+    secrets  = aws_iam_policy.lambda_secrets.arn
+    dynamodb = aws_iam_policy.lambda_dynamodb.arn
+    logs     = aws_iam_policy.lambda_logs.arn
   }
 }
 
@@ -65,20 +68,22 @@ output "ecs_execution_policy_arns" {
 output "ecs_task_policy_arns" {
   description = "ARNs de las políticas de ECS Task (aplicación)"
   value = {
-    dynamodb    = aws_iam_policy.ecs_dynamodb.arn
-    s3          = aws_iam_policy.ecs_s3.arn
-    sqs         = aws_iam_policy.ecs_sqs.arn
-    sns         = aws_iam_policy.ecs_sns.arn
-    secrets     = aws_iam_policy.ecs_secrets.arn
-    opensearch  = aws_iam_policy.ecs_opensearch.arn
-    cloudwatch  = aws_iam_policy.ecs_cloudwatch.arn
-    cloudmap    = aws_iam_policy.ecs_cloudmap.arn
+    dynamodb   = aws_iam_policy.ecs_dynamodb.arn
+    s3         = aws_iam_policy.ecs_s3.arn
+    sqs        = aws_iam_policy.ecs_sqs.arn
+    sns        = aws_iam_policy.ecs_sns.arn
+    secrets    = aws_iam_policy.ecs_secrets.arn
+    opensearch = aws_iam_policy.ecs_opensearch.arn
+    cloudwatch = aws_iam_policy.ecs_cloudwatch.arn
+    cloudmap   = aws_iam_policy.ecs_cloudmap.arn
   }
 }
 
+
 # OUTPUTS DE POLÍTICAS ADICIONALES
+
 
 output "vpc_flow_logs_policy_arn" {
   description = "ARN de la política para VPC Flow Logs"
-  value       = var.create_vpc_flow_logs_policy ? aws_iam_policy.vpc_flow_logs[0].arn : null
+  value       = var.enable_vpc_flow_logs ? aws_iam_policy.vpc_flow_logs[0].arn : null
 }
